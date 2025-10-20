3e7d4135f19d38ad9fb8e4d6822c30654c11dc33
adrien-tello <adriennathan.tellokombou@ictuniversity.edu.cm>
2025-10-20 15:44:09 +0100

    error rendle

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // or higher
   },
-  base: '/Magnum-Lima-Fish/'
 });
